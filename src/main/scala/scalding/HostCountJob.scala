190db5d7b969e8e9c1cbbdfa637602dc6d182ff8
Anitha <anithapraba@indix.com>
2015-08-27 15:24:19 +0530

    Added Scalding code and output for url filtering by host

---
+++ src/main/scala/scalding/HostCountJob.scala
@@ -0,0 +1,23 @@
+package scalding
+
+import com.twitter.scalding._
+import java.net.URL
+
+class HostCountJob(args : Args) extends Job(args) {
+  TextLine( args("input") )
+    .map('line -> 'host) { url : String => new URL(url).getHost()}
+    .groupBy('host) { _.size }
+    .filter('size) { count:Int => count >= 5}
+    .write( Tsv( args("output") ) )
+}
+
+object HostCountJob extends App {
+  val progargs: Array[String] = List(
+    "-Dmapred.map.tasks=200",
+    "scalding.HostCountJob",
+    "--input", "src/main/resources/sampleurls.txt",
+    "--output", "src/main/resources/scalding_output",
+    "--hdfs"
+  ).toArray
+  Tool.main(progargs)
+}
