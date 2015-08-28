366b495956456d013caaf93e7ae269d86f72796b
Anitha <anithapraba@indix.com>
2015-08-28 12:39:40 +0530

    Updated reduce function

--- src/main/scala/vanillaMR/HostCount.scala
+++ src/main/scala/vanillaMR/HostCount.scala
@@ -1,7 +1,7 @@
 package vanillaMR
 
 import org.apache.hadoop.fs.Path
-import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.io.{LongWritable, Text}
 import org.apache.hadoop.mapreduce.{Reducer, Job, Mapper}
 import org.apache.hadoop.conf.{Configured}
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
@@ -22,13 +22,13 @@ object HostCount extends Configured with Tool {
 
     job.setMapperClass(classOf[HostCountMapper])
     job.setMapOutputKeyClass(classOf[Text])
-    job.setMapOutputValueClass(classOf[IntWritable])
+    job.setMapOutputValueClass(classOf[LongWritable])
 
     job.setCombinerClass(classOf[HostCountReducer])
     job.setReducerClass(classOf[HostCountReducer])
 
     job.setOutputKeyClass(classOf[Text])
-    job.setOutputValueClass(classOf[IntWritable])
+    job.setOutputValueClass(classOf[LongWritable])
 
     FileInputFormat.addInputPath(job, new Path(args.head))
     FileOutputFormat.setOutputPath(job, new Path(args.last))
@@ -36,22 +36,27 @@ object HostCount extends Configured with Tool {
     if(job.waitForCompletion(true)) 0 else 1
   }
 
+  class HostCountMapper extends Mapper[LongWritable, Text, Text, LongWritable] {
+    protected override def map(lnNumber: LongWritable, line: Text, context: Mapper[LongWritable, Text, Text, LongWritable]#Context): Unit = {
+
+      context.write(new Text(new URL(line.toString).getHost),new LongWritable(1))
+    }
+  }
+
+
+  class HostCountReducer extends Reducer[Text, LongWritable, Text, LongWritable]{
+    protected override def reduce(key: Text, value: java.lang.Iterable[LongWritable], context: Reducer[Text, LongWritable, Text, LongWritable]#Context): Unit = {
+
+      val numURLsWithSameHost = sum(value.toList)
+      if( numURLsWithSameHost >= 5)
+        context.write(key,new LongWritable(numURLsWithSameHost))
+
+      def sum(l: List[LongWritable]): Long = l.foldLeft(0){(acc,sum)=> acc + sum.get().toInt}
+    }
+  }
+
   def main(args: Array[String]) {
     System.exit(ToolRunner.run(this, Array("src/main/resources/sampleurls.txt","src/main/resources/hostcount")))
   }
 
-  class HostCountMapper extends Mapper[IntWritable, Text, Text, IntWritable] {
-    protected override def map(lnNumber: IntWritable, line: Text, context: Mapper[IntWritable, Text, Text, IntWritable]#Context): Unit = {
-      context.write(new Text(new URL(line.toString).getHost),new IntWritable(1))
-    }
-  }
-
-
-  class HostCountReducer extends Reducer[Text, IntWritable, Text, IntWritable] {
-    protected override def reduce(key: Text, value: java.lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
-      val numURLsWithSameHost = new IntWritable(Integer.parseInt(value.reduceLeft(_ + _.toString)))
-      if( numURLsWithSameHost.get() >= 5)
-        context.write(key,numURLsWithSameHost)
-    }
-  }
 }
