ad8bfc4f20d7638d70f786f3499d1640231df43b
Anitha <anithapraba@indix.com>
2015-08-31 18:01:30 +0530

    Added JobTest for HostCountJob

---
+++ src/test/scala/scalding/HostCountJobTest.scala
@@ -0,0 +1,31 @@
+package scalding
+
+import com.twitter.scalding.{TupleConversions, Tsv, JobTest}
+import org.scalatest.{ShouldMatchers, FlatSpec}
+import scala.collection.mutable.Buffer
+import cascading.tuple.Fields
+
+
+class HostCountJobTest extends FlatSpec with ShouldMatchers with TupleConversions{
+
+  val fields = new Fields("host")
+
+  "url list" should "be grouped by hostname" in {
+
+    val validInput = "http://www.google.com/" :: "http://www.yahoo.com/" :: "http://www.google.com/" :: Nil
+
+    def verify(buffer: Buffer[(String, Int)]) {
+
+      buffer.size shouldEqual 2
+
+    }
+
+    JobTest(new HostCountJob(_))
+      .arg("input", "inputFile")
+      .source(Tsv("inputFile",fields), validInput)
+      .sink(Tsv("outputFile"))(verify)
+      .run
+      .finish
+  }
+
+}
